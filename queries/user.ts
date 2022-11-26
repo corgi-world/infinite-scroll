4859d602fa061675a045b9a2391f1f85e87ebbe6
corgi-world <developer.msw@gmail.com>
2022-11-26 12:34:31 +0900

    feat: user list infinite query 구현

---
+++ queries/user.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import type { IUserResponse } from "@/types/user";
+
+const service = axios.create({
+  baseURL: "/api/userList",
+});
+function get(queryString = "") {
+  return service.get(queryString).then((response) => response.data);
+}
+
+export function useFetchUserList() {
+  const queryResult = useInfiniteQuery<IUserResponse>(
+    ["user"],
+    ({ pageParam = "" }) => get(`?id=${pageParam}`),
+    {
+      getNextPageParam: ({ userList }) =>
+        userList ? userList[userList.length - 1].id : undefined,
+    }
+  );
+
+  return queryResult;
+}
