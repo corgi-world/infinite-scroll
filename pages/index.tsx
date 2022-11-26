327c20eca49c1f601cd4f6e12094638935f63c9c
corgi-world <developer.msw@gmail.com>
2022-11-26 12:35:39 +0900

    feat: 무한 스크롤 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,48 @@
 import React from "react";
+import styled from "styled-components";
+import User from "@/components/User";
+import { useFetchUserList } from "@/queries/user";
+import IntersectObserver from "@/components/IntersectObserver";
+import type { IUser } from "@/types/user";
 
-export default function index() {
-  return <div>index</div>;
+export default function Home() {
+  const { isFetching, data, hasNextPage, fetchNextPage } = useFetchUserList();
+
+  const handleIntersection = () => {
+    if (hasNextPage) {
+      fetchNextPage();
+    }
+  };
+
+  const renderUserList = () => {
+    if (data && data.pages) {
+      const userList = data.pages.reduce<IUser[]>((prev, { userList }) => {
+        if (userList) prev.push(...userList);
+        return prev;
+      }, []);
+
+      return userList.map(({ id, nickname }) => (
+        <User key={id} id={id} nickname={nickname} />
+      ));
+    }
+  };
+
+  return (
+    <Wrapper>
+      {renderUserList()}
+      {!isFetching && hasNextPage && (
+        <IntersectObserver handleIntersection={handleIntersection} />
+      )}
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  width: 100%;
+  max-width: 400px;
+  display: flex;
+  padding: 10px;
+  flex-direction: column;
+  gap: 10px;
+`;
